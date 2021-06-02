f37d41231122390e87b11b69e377a2d737efab3f
Gus Ferreyra <g.ferreyra@dxc.com>
2021-06-02 13:33:47 -0300

    Simple TODO app

--- src/App.js
+++ src/App.js
@@ -4,26 +4,6 @@ import React, {Component} from 'react';
 import {tareas} from './tareas.json';
  import TareaForm from './components/TareaForm';
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
 console.log(tareas);
 class App extends Component{
   constructor(){
